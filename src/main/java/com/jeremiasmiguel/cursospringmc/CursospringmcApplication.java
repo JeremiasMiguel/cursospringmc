5e638791e1ba0c8f3e4b1fc1477d4ccdf689658c
Jeremias Miguel <jjjeremiasmiguel@gmail.com>
2020-03-30 15:12:17 -0300

    Projeto criado

---
+++ src/main/java/com/jeremiasmiguel/cursospringmc/CursospringmcApplication.java
@@ -0,0 +1,13 @@
+package com.jeremiasmiguel.cursospringmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringmcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeremiasmiguel/cursospringmc/CursospringmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeremiasmiguel.cursospringmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
