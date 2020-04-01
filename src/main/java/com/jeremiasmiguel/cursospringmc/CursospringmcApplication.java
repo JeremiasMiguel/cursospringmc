236491bdd01067cbd5ec36610d22840448437559
Jeremias Miguel <jjjeremiasmiguel@gmail.com>
2020-04-01 11:43:49 -0300

    Criando operação de instanciação

--- src/main/java/com/jeremiasmiguel/cursospringmc/CursospringmcApplication.java
+++ src/main/java/com/jeremiasmiguel/cursospringmc/CursospringmcApplication.java
@@ -1,13 +1,33 @@
 package com.jeremiasmiguel.cursospringmc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.jeremiasmiguel.cursospringmc.domain.Categoria;
+import com.jeremiasmiguel.cursospringmc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursospringmcApplication {
+public class CursospringmcApplication implements CommandLineRunner {
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringmcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria categoria1 = new Categoria(null, "Informática");
+		Categoria categoria2 = new Categoria(null, "Escritório");
+		
+		// salvando no banco de dados, com o repository
+		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
 	}
 
 }
