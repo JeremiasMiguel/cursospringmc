6ac8c3d3fa52238640cdd0bc3ceaa16e542875b4
Jeremias Miguel <jjjeremiasmiguel@gmail.com>
2020-04-09 18:48:24 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/jeremiasmiguel/cursospringmc/resources/ClienteResource.java
+++ src/main/java/com/jeremiasmiguel/cursospringmc/resources/ClienteResource.java
@@ -1,13 +1,22 @@
 package com.jeremiasmiguel.cursospringmc.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jeremiasmiguel.cursospringmc.domain.Cliente;
+import com.jeremiasmiguel.cursospringmc.dto.ClienteDTO;
 import com.jeremiasmiguel.cursospringmc.services.ClienteService;
 
 // indicando que a classe é um controlador REST
@@ -24,6 +33,45 @@ public class ClienteResource {
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) { // indicando que o ID da URL vai ter que ir pro id variável
 		Cliente objetoCliente= clienteService.find(id);
 		return ResponseEntity.ok().body(objetoCliente);
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO clienteDTO, @PathVariable Integer id) {
+		Cliente cliente = this.clienteService.fromDTO(clienteDTO);
+		cliente.setId(id);
+		cliente = clienteService.update(cliente);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Cliente> delete(@PathVariable Integer id) { // indicando que o ID da URL vai ter que ir pro id variável
+		clienteService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	// Retornando todas as clientes (endpoint: /clientes)
+	// Usando DTO para retornar somente os dados realmente necessários (id e nome - sem os produtos)
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() { 
+		List<Cliente> listaClientes = clienteService.findAll();
+		// Convertendo a lista de Categorias para uma lista de CategoriasDTO
+		List<ClienteDTO> listaClientesDTO = listaClientes.stream().map(objetoCliente -> new ClienteDTO(objetoCliente)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listaClientesDTO);
+	}
+	
+	/* Criando um método semelhante ao findAll, com a diferença de busca por página, e a criação de um novo
+	   endpoint (clientes/page) e a passagem dos dados será por parâmetros opcionais. 
+	   Exemplo: localhost/clientes/page?page=1&linesPerPage=20*/
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) { 
+		Page<Cliente> listaClientes = clienteService.findPage(page, linesPerPage, orderBy, direction);
+		// Convertendo a lista de Categorias para uma lista de CategoriasDTO
+		Page<ClienteDTO> listaClientesDTO = listaClientes.map(objetoCliente -> new ClienteDTO(objetoCliente));
+		return ResponseEntity.ok().body(listaClientesDTO);
 	}
 	
 }
--- src/main/java/com/jeremiasmiguel/cursospringmc/services/ClienteService.java
+++ src/main/java/com/jeremiasmiguel/cursospringmc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.jeremiasmiguel.cursospringmc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.jeremiasmiguel.cursospringmc.domain.Cliente;
+import com.jeremiasmiguel.cursospringmc.dto.ClienteDTO;
 import com.jeremiasmiguel.cursospringmc.repositories.ClienteRepository;
+import com.jeremiasmiguel.cursospringmc.services.exceptions.DataIntegrityException;
 import com.jeremiasmiguel.cursospringmc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -22,4 +29,50 @@ public class ClienteService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())
 		);
 	} 
+	
+	// O método SAVE serve tanto para inserir como para atualizar, a única diferença é o ID,
+		// se for nulo, ele adiciona, se não for, ele atualiza
+		public Cliente update(Cliente cliente) {
+			// Verificando se o ID pesquisado para alteração existe ou não é nulo
+			Cliente novoCliente = this.find(cliente.getId());
+			// Atualizando o novoCliente com os dados que podem ser atualizados (nome e email)
+			updateData(novoCliente, cliente);
+			return clienteRepository.save(novoCliente);
+		}
+		
+		public void delete(Integer id) {
+			this.find(id);
+			try {
+				clienteRepository.deleteById(id);
+			}
+			/* Exceção causada pela tentativa de exclusão de uma entidade que contém outras relacionadas, 
+			   por exemplo, a entidade Cliente pode conter entidades relacionados a ele, se houver,
+			   a exclusão é abortada por meio dessa captura de uma exceção personalizada criada (DataIntegrityException) */
+			catch(DataIntegrityViolationException exception) {
+				throw new DataIntegrityException("Não é possível realizar a exclusão de um Cliente que contenha entidades relacionadas.");
+			}
+		}
+		
+		public List<Cliente> findAll() {
+			return clienteRepository.findAll();
+		}
+		
+		/* Função que define uma paginação de dados, onde é possível definir a página que se deseja verificar,
+		   quantas linhas serão mostradas por página, o atributo pelo qual se ordenará e a ordenação dos dados (asc ou desc)
+		 */
+		public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+			PageRequest pageRequest =  PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+			// Busca dos dados no BD seguindo os preceitos do PageRequest, retornando uma Page
+			return clienteRepository.findAll(pageRequest);
+		}
+		
+		// Convertendo uma Cliente em ClienteDTO
+		public Cliente fromDTO(ClienteDTO clienteDTO) {
+			return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
+		}
+		
+		private void updateData(Cliente novoCliente, Cliente cliente) {
+			novoCliente.setNome(cliente.getNome());
+			novoCliente.setEmail(cliente.getEmail());
+		}
 }
