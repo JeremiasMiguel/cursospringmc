94788b76e6ff7defa58cf68fcbc2dc4561d7d8ba
Jeremias Miguel <jjjeremiasmiguel@gmail.com>
2020-04-09 19:10:26 -0300

    Correções: update em CategoriaService e construtor de Pagamento (enum)

--- src/main/java/com/jeremiasmiguel/cursospringmc/services/CategoriaService.java
+++ src/main/java/com/jeremiasmiguel/cursospringmc/services/CategoriaService.java
@@ -26,51 +26,63 @@ public class CategoriaService {
 	public Categoria find(Integer id) {
 		Optional<Categoria> objetoCategoria = categoriaRepository.findById(id);
 		return objetoCategoria.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())
-		);
-	} 
-	
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
 	public Categoria insert(Categoria categoria) {
 		categoria.setId(null);
 		return categoriaRepository.save(categoria);
 	}
-	
+
 	// O método SAVE serve tanto para inserir como para atualizar, a única diferença é o ID,
 	// se for nulo, ele adiciona, se não for, ele atualiza
 	public Categoria update(Categoria categoria) {
 		// Verificando se o ID pesquisado para alteração existe ou não é nulo
-		this.find(categoria.getId());
-		return categoriaRepository.save(categoria);
+		Categoria novaCategoria = this.find(categoria.getId());
+		// Atualizando o novoCliente com os dados que podem ser atualizados (nome e email)
+		updateData(novaCategoria, categoria);
+		return categoriaRepository.save(novaCategoria);
 	}
-	
+
 	public void delete(Integer id) {
 		this.find(id);
 		try {
 			categoriaRepository.deleteById(id);
 		}
-		/* Exceção causada pela tentativa de exclusão de uma entidade que contém outras relacionadas, 
-		   por exemplo, a entidade Categoria pode conter Produtos relacionados a ela, se houver,
-		   a exclusão é abortada por meio dessa captura de uma exceção personalizada criada (DataIntegrityException) */
-		catch(DataIntegrityViolationException exception) {
-			throw new DataIntegrityException("Não é possível realizar a exclusão de uma Categoria que contenha Produtos.");
+		/*
+		 * Exceção causada pela tentativa de exclusão de uma entidade que contém outras
+		 * relacionadas, por exemplo, a entidade Categoria pode conter Produtos
+		 * relacionados a ela, se houver, a exclusão é abortada por meio dessa captura
+		 * de uma exceção personalizada criada (DataIntegrityException)
+		 */
+		catch (DataIntegrityViolationException exception) {
+			throw new DataIntegrityException(
+					"Não é possível realizar a exclusão de uma Categoria que contenha Produtos.");
 		}
 	}
-	
+
 	public List<Categoria> findAll() {
 		return categoriaRepository.findAll();
 	}
-	
-	/* Função que define uma paginação de dados, onde é possível definir a página que se deseja verificar,
-	   quantas linhas serão mostradas por página, o atributo pelo qual se ordenará e a ordenação dos dados (asc ou desc)
+
+	/*
+	 * Função que define uma paginação de dados, onde é possível definir a página
+	 * que se deseja verificar, quantas linhas serão mostradas por página, o
+	 * atributo pelo qual se ordenará e a ordenação dos dados (asc ou desc)
 	 */
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
-		PageRequest pageRequest =  PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		// Busca dos dados no BD seguindo os preceitos do PageRequest, retornando uma Page
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		// Busca dos dados no BD seguindo os preceitos do PageRequest, retornando uma
+		// Page
 		return categoriaRepository.findAll(pageRequest);
 	}
-	
+
 	// Convertendo uma Categoria em CategoriaDTO
 	public Categoria fromDTO(CategoriaDTO categoriaDTO) {
 		return new Categoria(categoriaDTO.getId(), categoriaDTO.getNome());
+	}
+	
+	private void updateData(Categoria novaCategoria, Categoria categoria) {
+		novaCategoria.setNome(categoria.getNome());
 	}
 }
--- src/main/java/com/jeremiasmiguel/cursospringmc/services/ClienteService.java
+++ src/main/java/com/jeremiasmiguel/cursospringmc/services/ClienteService.java
@@ -24,55 +24,61 @@ public class ClienteService {
 	private ClienteRepository clienteRepository;
 
 	public Cliente find(Integer id) {
-		 Optional<Cliente> objetoCliente = clienteRepository.findById(id);
+		Optional<Cliente> objetoCliente = clienteRepository.findById(id);
 		return objetoCliente.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())
-		);
-	} 
-	
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
 	// O método SAVE serve tanto para inserir como para atualizar, a única diferença é o ID,
-		// se for nulo, ele adiciona, se não for, ele atualiza
-		public Cliente update(Cliente cliente) {
-			// Verificando se o ID pesquisado para alteração existe ou não é nulo
-			Cliente novoCliente = this.find(cliente.getId());
-			// Atualizando o novoCliente com os dados que podem ser atualizados (nome e email)
-			updateData(novoCliente, cliente);
-			return clienteRepository.save(novoCliente);
+	// se for nulo, ele adiciona, se não for, ele atualiza
+	public Cliente update(Cliente cliente) {
+		// Verificando se o ID pesquisado para alteração existe ou não é nulo
+		Cliente novoCliente = this.find(cliente.getId());
+		// Atualizando o novoCliente com os dados que podem ser atualizados (nome e email)
+		updateData(novoCliente, cliente);
+		return clienteRepository.save(novoCliente);
+	}
+
+	public void delete(Integer id) {
+		this.find(id);
+		try {
+			clienteRepository.deleteById(id);
 		}
-		
-		public void delete(Integer id) {
-			this.find(id);
-			try {
-				clienteRepository.deleteById(id);
-			}
-			/* Exceção causada pela tentativa de exclusão de uma entidade que contém outras relacionadas, 
-			   por exemplo, a entidade Cliente pode conter entidades relacionados a ele, se houver,
-			   a exclusão é abortada por meio dessa captura de uma exceção personalizada criada (DataIntegrityException) */
-			catch(DataIntegrityViolationException exception) {
-				throw new DataIntegrityException("Não é possível realizar a exclusão de um Cliente que contenha entidades relacionadas.");
-			}
-		}
-		
-		public List<Cliente> findAll() {
-			return clienteRepository.findAll();
-		}
-		
-		/* Função que define uma paginação de dados, onde é possível definir a página que se deseja verificar,
-		   quantas linhas serão mostradas por página, o atributo pelo qual se ordenará e a ordenação dos dados (asc ou desc)
+		/*
+		 * Exceção causada pela tentativa de exclusão de uma entidade que contém outras
+		 * relacionadas, por exemplo, a entidade Cliente pode conter entidades
+		 * relacionados a ele, se houver, a exclusão é abortada por meio dessa captura
+		 * de uma exceção personalizada criada (DataIntegrityException)
 		 */
-		public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
-			PageRequest pageRequest =  PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-			// Busca dos dados no BD seguindo os preceitos do PageRequest, retornando uma Page
-			return clienteRepository.findAll(pageRequest);
+		catch (DataIntegrityViolationException exception) {
+			throw new DataIntegrityException(
+					"Não é possível realizar a exclusão de um Cliente que contenha entidades relacionadas.");
 		}
-		
-		// Convertendo uma Cliente em ClienteDTO
-		public Cliente fromDTO(ClienteDTO clienteDTO) {
-			return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
-		}
-		
-		private void updateData(Cliente novoCliente, Cliente cliente) {
-			novoCliente.setNome(cliente.getNome());
-			novoCliente.setEmail(cliente.getEmail());
-		}
+	}
+
+	public List<Cliente> findAll() {
+		return clienteRepository.findAll();
+	}
+
+	/*
+	 * Função que define uma paginação de dados, onde é possível definir a página
+	 * que se deseja verificar, quantas linhas serão mostradas por página, o
+	 * atributo pelo qual se ordenará e a ordenação dos dados (asc ou desc)
+	 */
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		// Busca dos dados no BD seguindo os preceitos do PageRequest, retornando uma
+		// Page
+		return clienteRepository.findAll(pageRequest);
+	}
+
+	// Convertendo uma Cliente em ClienteDTO
+	public Cliente fromDTO(ClienteDTO clienteDTO) {
+		return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
+	}
+
+	private void updateData(Cliente novoCliente, Cliente cliente) {
+		novoCliente.setNome(cliente.getNome());
+		novoCliente.setEmail(cliente.getEmail());
+	}
 }
