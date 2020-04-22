72b052fb6833c1755ea6d05fc9549c54fffacfc5
Jeremias Miguel <jjjeremiasmiguel@gmail.com>
2020-04-22 14:11:23 -0300

    Refresh token

---
+++ src/main/java/com/jeremiasmiguel/cursospringmc/resources/AuthResource.java
@@ -0,0 +1,36 @@
+package com.jeremiasmiguel.cursospringmc.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jeremiasmiguel.cursospringmc.security.JWTUtil;
+import com.jeremiasmiguel.cursospringmc.security.UserSpringSecurity;
+import com.jeremiasmiguel.cursospringmc.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+	@Autowired
+	JWTUtil jwtUtil;
+	
+	/*
+	 * Método que gera outro token de autenticação quando um
+	 * antigo estiver perto de sua expiração, e mantém o
+	 * usuário conectado. Logicamente o usuário deve estar logado
+	 * para acessar esse endpoint
+	 */
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSpringSecurity user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+
+}
